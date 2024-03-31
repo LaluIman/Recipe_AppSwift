55415dca1c8266625715098c86be49f35a7ed7f2
Eye-Mans <laluiman468@gmail.com>
2024-03-31 22:01:03 +0800

    Add searchbar

--- Ummi Recipe/Views/RecipeView.swift
+++ Ummi Recipe/Views/RecipeView.swift
@@ -8,56 +8,78 @@
 import SwiftUI
 
 struct RecipeView: View {
-    @ObservedObject var recipeManager: RecipeManager
-    @State var selectedRecipe: Recipe? = nil
-    var body: some View {
-        NavigationView{
-            ScrollView(.vertical, showsIndicators: false){
-                VStack(alignment: .leading){
-//                    VStack {
-//                        Text("")
-//                            .font(.system(size: 40))
-//                            .fontWeight(.bold)
-//                            .multilineTextAlignment(.leading)
-//                    }
-//                    .padding()
-                    
-                    ForEach(recipeManager.recipes.shuffled()){ recipe in
-                        RecipeCard(recipe: recipe)
-                            .onTapGesture {
-                                selectedRecipe = recipe
-                            }
-                    }
-                    .padding(.bottom, 15)
-                    .padding(.horizontal)
-                    .fullScreenCover(item: $selectedRecipe) { recipe in
-                        RecipeDetailView(recipe: recipe)
-                    }
-                }
-                .padding(.vertical,3)
-            }
-            .background(.ultraThickMaterial)
-            .navigationTitle("Recipes🍳")
-            .navigationBarItems(leading: refreshButton)
+  @ObservedObject var recipeManager: RecipeManager
+  @State var selectedRecipe: Recipe? = nil
+  @State private var searchTerm: String = ""
+
+  var body: some View {
+    NavigationView {
+      ScrollView(.vertical, showsIndicators: false) {
+        VStack(alignment: .center) {
+          VStack {
+            Text("Welcome Cookers!")
+              .foregroundStyle(.green)
+              .font(.system(size: 37))
+              .fontWeight(.bold)
+            Text("Make the food, Because you are")
+              .fontWeight(.medium)
+            Text("food warrior ⚔!")
+              .fontWeight(.bold)
+              HStack{
+                  TextField("Search Recipes", text: $searchTerm)
+                  Image(systemName: "magnifyingglass")
+              }
+              .textFieldStyle(RoundedBorderTextFieldStyle())
+              .foregroundColor(.black)
+              .padding(.horizontal, 30)
+          }
+          .padding(.vertical, 20)
+          .padding(.bottom, 20)
+
+
+          ForEach(searchTerm.isEmpty ? recipeManager.recipes : recipeManager.recipes.filter {
+            $0.title.lowercased().contains(searchTerm.lowercased())
+          }) { recipe in
+            RecipeCard(recipe: recipe)
+              .onTapGesture {
+                selectedRecipe = recipe
+              }
+          }
+          .padding(.bottom, 15)
+          .padding(.horizontal)
+          .fullScreenCover(item: $selectedRecipe) { recipe in
+            RecipeDetailView(recipe: recipe)
+          }
         }
+        .padding(.vertical, 3)
+      }
+      .background(.ultraThickMaterial)
+      .navigationBarItems(trailing: refreshButton)
     }
-    
-    var refreshButton: some View {
-       Button(action: {
-         recipeManager.recipes.shuffle()
-       }) {
-           ZStack{
-               Image(systemName: "arrow.clockwise.circle")
-                   .font(.title)
-                   .foregroundStyle(.white)
-               Image(systemName: "arrow.clockwise.circle.fill")
-                     .font(.title)
-                     .foregroundColor(.green)
-           }
-       }
-     }
-   }
+  }
+
+  var refreshButton: some View {
+    Button(action: {
+      recipeManager.recipes.shuffle()
+    }) {
+      HStack(spacing: 0.2) {
+        Text("Refresh")
+          .font(.system(size: 20))
+          .foregroundStyle(.green)
+          .fontWeight(.medium)
+        ZStack {
+          Image(systemName: "arrow.clockwise.circle")
+            .font(.title2)
+            .foregroundStyle(.white)
+          Image(systemName: "arrow.clockwise.circle.fill")
+            .font(.title2)
+            .foregroundColor(.green)
+        }
+      }
+    }
+  }
+}
 
 #Preview {
-    RecipeView(recipeManager: RecipeManager())
+  RecipeView(recipeManager: RecipeManager())
 }
